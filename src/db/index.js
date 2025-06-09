133dfe1cbb315ab9293e03ca266f2dccf5228452
DawoodIfzaal <dawoodifzal.di2@gmail.com>
2025-06-09 13:13:51 +0530

    add listen

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,6 @@ import { DB_NAME } from "../constants.js"
 
 
 const connectDB = async () => {
-  console.log(process.env.MONGODB_URL)
   try{
     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
     console.log("MongoDB connection sucessfull")
--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import "dotenv/config"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 connectDB()
+.then(() => {
+  const server = app.listen(process.env.PORT || 4000, () => { 
+    console.log(`server is running at PORT = ${process.env.PORT}`)
+  })
+
+  server.on('error', (error) => {
+    console.log('server startup error', error)
+    process.exit(1)
+  })
+})
