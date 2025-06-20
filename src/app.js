9217c5381466e1f8b8ada5cd96a88610112b1f47
DawoodIfzaal <dawoodifzal.di2@gmail.com>
2025-06-20 16:19:55 +0530

    finish comment endpoints

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 //route declaration
 app.use("/api/v1/users", userRouter)
@@ -35,5 +36,6 @@ app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/playlists", playlistRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/comments", commentRouter)
 
 export {app}
--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -47,10 +47,111 @@ const addComment = asyncHandler(async (req, res) => {
       comment,
       "commented on a video successfully"
     )
-  )
-  
+  )  
 })
 
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params
+  const { content } = req.body
+
+  if(!content?.trim()){
+    throw new ApiError(400, "Comment cannot be empty")
+  }
+
+  if (!isValid(commentId)) {
+    throw new ApiError(400, "Invalid comment ID")
+  }
+
+  const comment = await Comment.findById(commentId)
+  
+  if (!comment) {
+    throw new ApiError(404, "Comment not found")
+  }
+
+  if (!comment.owner.equals(req.user._id)) {
+    throw new ApiError(403, "You are not the owner of this comment")
+  }
+
+  comment.content = content.trim()
+  await comment.save()
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200, 
+      comment, 
+      "Comment deleted successfully"
+    )
+  )
+})
+
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params
+
+  if (!isValid(commentId)) {
+    throw new ApiError(400, "Invalid comment ID")
+  }
+
+  const comment = await Comment.findById(commentId)
+  
+  if (!comment) {
+    throw new ApiError(404, "Comment not found")
+  }
+
+  if (!comment.owner.equals(req.user._id)) {
+    throw new ApiError(403, "You are not the owner of this comment")
+  }
+
+  await comment.deleteOne()
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200, 
+      {}, 
+      "Comment deleted successfully"
+    )
+  )
+})
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const {videoId} = req.params
+  const {page = 1, limit = 10} = req.query
+
+  if(!isValid(videoId)){
+    throw new ApiError(400, "Invalid videoId format")
+  }
+
+  const video = await Video.findById(videoId)
+
+  if(!video){
+    throw new ApiError(404, "video not found")
+  }
+
+  const comments = await Comment.find({ video: videoId })
+    .populate("owner", "username fullName avatar") 
+    .sort({ createdAt: -1 }) //latest first
+    .skip((page - 1) * limit)
+    .limit(parseInt(limit));
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      comments,
+      "video comments fetched successfully"
+    )
+  )
+})
+
+
 export{
-  addComment
+  addComment,
+  updateComment,
+  deleteComment,
+  getVideoComments
 }
