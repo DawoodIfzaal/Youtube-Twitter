d4fcb39d2bf80422ef8cb91700f9146fadbe3948
DawoodIfzaal <dawoodifzal.di2@gmail.com>
2025-06-21 11:32:58 +0530

    add healthcheck endpoint

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ import playlistRouter from "./routes/playlist.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 //route declaration
 app.use("/api/v1/users", userRouter)
@@ -39,5 +40,6 @@ app.use("/api/v1/playlists", playlistRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export {app}
